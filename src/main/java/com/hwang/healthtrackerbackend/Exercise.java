5f013d50e81235e1149054eb774e5ad78f3a451c
dhwang21 <hwang.derrick@gmail.com>
2019-01-24 11:06:49 -0800

    complete database setup

---
+++ src/main/java/com/hwang/healthtrackerbackend/Exercise.java
@@ -0,0 +1,75 @@
+package com.hwang.healthtrackerbackend;
+
+import javax.annotation.Generated;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+
+@Entity
+public class Exercise {
+
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Id
+    public int id;
+
+    public String getTitle() {
+        return title;
+    }
+
+    public void setTitle(String title) {
+        this.title = title;
+    }
+
+    public int getSets() {
+        return sets;
+    }
+
+    public void setSets(int sets) {
+        this.sets = sets;
+    }
+
+    public int getReps() {
+        return reps;
+    }
+
+    public void setReps(int reps) {
+        this.reps = reps;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
+
+    public String getTimestamp() {
+        return timestamp;
+    }
+
+    public void setTimestamp(String timestamp) {
+        this.timestamp = timestamp;
+    }
+
+    public String title;
+    public int sets;
+    public int reps;
+    public String description;
+    public String timestamp;
+
+    public Exercise(String title, int sets, int reps, String description, String timestamp){
+        this.title = title;
+        this.sets = sets;
+        this.reps = reps;
+        this.description = description;
+        this.timestamp = timestamp;
+    }
+
+    public Exercise(){}
+
+    public String toString(){
+        return "Exercise name: " + title + " Sets: " + sets + " Reps: " + reps + " Description: " + description + " Time: " + timestamp;
+    }
+}
