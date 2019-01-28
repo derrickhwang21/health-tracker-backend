837a23e55c9308b1010b5e7e26c5dc05be12c0ce
dhwang21 <hwang.derrick@gmail.com>
2019-01-27 19:17:34 -0800

    update model

--- src/main/java/com/hwang/healthtrackerbackend/ExerciseController.java
+++ src/main/java/com/hwang/healthtrackerbackend/ExerciseController.java
@@ -32,19 +32,21 @@ public class ExerciseController {
                             @RequestParam String sets,
                             @RequestParam String reps,
                             @RequestParam String description,
-                            @RequestParam String timestamp){
+                            @RequestParam String timestamp,
+                            @RequestParam String longitude,
+                            @RequestParam String latitude){
         if(timestamp == null){
             Date currentTime = Calendar.getInstance().getTime();
             timestamp = currentTime.toString();
         }
-        Exercise newExerciseObject = new Exercise(title,  Integer.parseInt(sets), Integer.parseInt(reps), description, timestamp);
+        Exercise newExerciseObject = new Exercise(title,  Integer.parseInt(sets), Integer.parseInt(reps), description, timestamp, Double.parseDouble(longitude), Double.parseDouble(latitude));
         return exerciseRepo.save(newExerciseObject);
     }
 
     @GetMapping(value = "/exercises/reset")
     public RedirectView resetParkingLot() {
         Date currentTime = Calendar.getInstance().getTime();
-        exerciseRepo.save(new Exercise("Server-Burpees", 1, 10, "All Day!", currentTime.toString()));
+        exerciseRepo.save(new Exercise("Server-Burpees", 1, 10, "All Day!", currentTime.toString(), 0.0, 0.0));
 
         return new RedirectView("/exercises");
     }
